b31714ff96c71e4f21b08baf1cb2d8a20fced842
Azhgaliev <Sport-titan@mail.ru>
2020-07-14 14:42:43 +0600

    add logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Todo simple';
   todos = [
-    {label: 'Add something'}
+    {label: ''}
   ]
 
   addTodo(newToDoLabel){
