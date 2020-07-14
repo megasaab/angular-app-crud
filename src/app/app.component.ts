d9bf498ef0e725b8594b2d110e4748cee7d891d5
Azhgaliev <Sport-titan@mail.ru>
2020-07-14 22:45:52 +0600

    fix del button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,28 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Angular cards';
-  Cards = [
-    {label:'testing'}
-  ]
+  Cards = []
 
   addCard(elem) {
+    let uid = function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
     var newCard = {
       label: elem,
-      text: 'angular mini card'
+      text: 'angular mini card',
+      id: uid(),
     };
    this.Cards.push(newCard)
   }
 
   deleteCard(card) {
-    this.Cards= this.Cards.filter(el => el.label !== card.label)
+    this.Cards= this.Cards.filter(t => t.id !== card.id)
+    console.log(card)
   }
+
+
 }
  
